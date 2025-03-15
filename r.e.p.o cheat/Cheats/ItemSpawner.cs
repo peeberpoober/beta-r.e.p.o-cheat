1464fe751a55bade45d8e59b2e91f78c266d5364
hiccup444 <sunnylilttledragon@gmail.com>
2025-03-15 16:12:53 -0400

    Update ItemSpawner.cs

--- r.e.p.o cheat/Cheats/ItemSpawner.cs
+++ r.e.p.o cheat/Cheats/ItemSpawner.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
- using Photon.Pun;
- using System.Collections.Generic;
- 
- public class ItemSpawner : MonoBehaviourPunCallbacks
- {
-     public static void SpawnItem(Vector3 position, int value = 45000)
+using UnityEngine;
+using Photon.Pun;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class ItemSpawner : MonoBehaviourPunCallbacks
+{
+    public static void SpawnItem(Vector3 position, int value = 45000)
     {
         try
         {
@@ -22,7 +24,7 @@
             if (!isMultiplayer)
             {
                 Debug.Log("Offline mode: Spawning locally.");
-                spawnedItem = UnityEngine.Object.Instantiate<GameObject>(itemPrefab, position, Quaternion.identity);
+                spawnedItem = Instantiate(itemPrefab, position, Quaternion.identity);
                 ConfigureSyncComponents(spawnedItem);
                 EnsureItemVisibility(spawnedItem);
             }
@@ -30,16 +32,9 @@
             {
                 Debug.Log("Multiplayer mode: Spawning via Photon.");
 
-                // Get the PhotonNetwork type as it is a static class
                 var photonNetworkType = typeof(PhotonNetwork);
-
-                // Get the NetworkInstantiate method which is internal and static
-                // Specify the exact parameter types to avoid ambiguity
-                var instantiateMethod = photonNetworkType.GetMethod("NetworkInstantiate",
-                    BindingFlags.NonPublic | BindingFlags.Static,
-                    null,
-                    new Type[] { typeof(InstantiateParameters), typeof(bool), typeof(bool) },
-                    null);
+                var instantiateMethod = photonNetworkType.GetMethod("NetworkInstantiate", BindingFlags.NonPublic | BindingFlags.Static, null,
+                    new Type[] { typeof(InstantiateParameters), typeof(bool), typeof(bool) }, null);
 
                 if (instantiateMethod == null)
                 {
@@ -47,10 +42,7 @@
                     return;
                 }
 
-                // Get the value of the currentLevelPrefix field
-                var currentLevelPrefixField = photonNetworkType.GetField("currentLevelPrefix",
-                    BindingFlags.NonPublic | BindingFlags.Static);
-
+                var currentLevelPrefixField = photonNetworkType.GetField("currentLevelPrefix", BindingFlags.NonPublic | BindingFlags.Static);
                 if (currentLevelPrefixField == null)
                 {
                     Debug.LogError("currentLevelPrefix field not found");
@@ -59,118 +51,141 @@
 
                 var currentLevelPrefix = currentLevelPrefixField.GetValue(null);
 
-                // Create the instantiation parameters - PASSING ITEM DATA WITH VALUE
-                var parameters = new InstantiateParameters("Valuables/" + itemPrefab.name,
+                var parameters = new InstantiateParameters(
+                    "Valuables/" + itemPrefab.name,
                     position,
                     Quaternion.identity,
                     0,
-                    itemData, // Pass the value as instantiation data
+                    itemData,
                     (byte)currentLevelPrefix,
                     null,
                     PhotonNetwork.LocalPlayer,
                     PhotonNetwork.ServerTimestamp);
 
-                // Invoke the method (pass null as the first parameter because it is a static method)
                 spawnedItem = (GameObject)instantiateMethod.Invoke(null, new object[] { parameters, true, false });
-
-                // Configure sync components
                 ConfigureSyncComponents(spawnedItem);
             }
 
-            // Step 2: Configure the valuable object component
-            // Since we're now passing the value via InstantiationData, we need to ensure
-            // it's properly set in ValuableObject even in singleplayer mode or as a fallback
-            var valuableComponent = spawnedItem.GetComponent(Type.GetType("ValuableObject, Assembly-CSharp"));
-            if (valuableComponent != null)
-            {
-                Debug.Log("ValuableObject component found");
-
-                // Set dollar values directly using reflection
-                var dollarValueOverride = valuableComponent.GetType().GetField("dollarValueOverride", BindingFlags.NonPublic | BindingFlags.Instance);
-                if (dollarValueOverride != null)
-                    dollarValueOverride.SetValue(valuableComponent, value);
-
-                var dollarValueOriginal = valuableComponent.GetType().GetField("dollarValueOriginal", BindingFlags.Public | BindingFlags.Instance);
-                if (dollarValueOriginal != null)
-                    dollarValueOriginal.SetValue(valuableComponent, (float)value);
-
-                var dollarValueCurrent = valuableComponent.GetType().GetField("dollarValueCurrent", BindingFlags.Public | BindingFlags.Instance);
-                if (dollarValueCurrent != null)
-                    dollarValueCurrent.SetValue(valuableComponent, (float)value);
-
-                var dollarValueSet = valuableComponent.GetType().GetField("dollarValueSet", BindingFlags.NonPublic | BindingFlags.Instance);
-                if (dollarValueSet != null)
-                    dollarValueSet.SetValue(valuableComponent, true);
-
-                // Use RPC method for network synchronization
-                var dollarValueRPC = valuableComponent.GetType().GetMethod("DollarValueSetRPC", BindingFlags.Public | BindingFlags.Instance);
-                if (dollarValueRPC != null)
-                {
-                    dollarValueRPC.Invoke(valuableComponent, new object[] { (float)value });
-
-                    // Synchronize with network clients if in multiplayer
-                    if (isMultiplayer)
-                    {
-                        var photonView = valuableComponent.GetComponent<PhotonView>();
-                        if (photonView != null)
-                        {
-                            photonView.RequestOwnership();
-                            Debug.Log($"Requested ownership for spawned item: {spawnedItem.name}");
-                        }
-                        photonView.RPC("DollarValueSetRPC", RpcTarget.Others, (float)value);
-                        photonView.RPC("DiscoverRPC", RpcTarget.All, Array.Empty<object>());
-                        photonView.RPC("AddToDollarHaulListRPC", RpcTarget.All, Array.Empty<object>());
-                    }
-                }
-                else
-                {
-                    Debug.LogError("DollarValueSetRPC method not found");
-                }
-            }
-            else
-            {
-                Debug.LogError("ValuableObject component not found");
-            }
-
-            // Step 3: Configure physics properties
-            var physComponent = spawnedItem.GetComponent(Type.GetType("PhysGrabObject, Assembly-CSharp"));
-            if (physComponent != null)
-            {
-                Debug.Log("PhysGrabObject component found");
-
-                // Set random torque
-                FieldInfo spawnTorqueField = physComponent.GetType().GetField("spawnTorque",
-                    BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
-
-                if (spawnTorqueField != null)
-                {
-                    Vector3 randomTorque = UnityEngine.Random.insideUnitSphere * 0.05f;
-                    spawnTorqueField.SetValue(physComponent, randomTorque);
-                    Debug.Log($"Set spawnTorque to {randomTorque}");
-
-                    // Sync position in multiplayer
-                    if (isMultiplayer)
-                    {
-                        var photonView = spawnedItem.GetComponent<PhotonView>();
-                        if (photonView != null)
-                        {
-                            photonView.RPC("SetPositionRPC", RpcTarget.MasterClient,
-                                new object[] { position, Quaternion.identity });
-                        }
-                    }
-                }
-                else
-                {
-                    Debug.LogError("spawnTorque field not found");
-                }
-            }
-            else
-            {
-                Debug.LogError("PhysGrabObject component not found");
-            }
+            ConfigureValuableObject(spawnedItem, value, isMultiplayer);
+            ConfigurePhysicsProperties(spawnedItem, position, isMultiplayer);
         }
         catch (Exception ex)
         {
             Debug.LogError($"Error in SpawnItem: {ex.Message}\n{ex.StackTrace}");
         }
     }
+
+    private static void ConfigureSyncComponents(GameObject item)
+    {
+        PhotonView pv = item.GetComponent<PhotonView>() ?? item.AddComponent<PhotonView>();
+        pv.ViewID = PhotonNetwork.AllocateViewID(0);
+        Debug.Log("PhotonView added to item: " + pv.ViewID);
+
+        PhotonTransformView transformView = item.GetComponent<PhotonTransformView>() ?? item.AddComponent<PhotonTransformView>();
+        Debug.Log("PhotonTransformView added to item");
+
+        Rigidbody rb = item.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            PhotonRigidbodyView rigidbodyView = item.GetComponent<PhotonRigidbodyView>() ?? item.AddComponent<PhotonRigidbodyView>();
+            rigidbodyView.m_SynchronizeVelocity = true;
+            rigidbodyView.m_SynchronizeAngularVelocity = true;
+            Debug.Log("PhotonRigidbodyView added and configured on item");
+        }
+
+        if (item.GetComponent<ItemSync>() == null)
+        {
+            item.AddComponent<ItemSync>();
+        }
+
+        pv.ObservedComponents = new List<Component> { transformView };
+        if (rb != null)
+        {
+            PhotonRigidbodyView rigidbodyView = item.GetComponent<PhotonRigidbodyView>();
+            if (rigidbodyView != null)
+            {
+                pv.ObservedComponents.Add(rigidbodyView);
+            }
+        }
+        pv.Synchronization = ViewSynchronization.ReliableDeltaCompressed;
+        EnsureItemVisibility(item);
+    }
+
+    private static void ConfigureValuableObject(GameObject spawnedItem, int value, bool isMultiplayer)
+    {
+        var valuableComponent = spawnedItem.GetComponent(Type.GetType("ValuableObject, Assembly-CSharp"));
+        if (valuableComponent == null)
+        {
+            Debug.LogError("ValuableObject component not found");
+            return;
+        }
+
+        Debug.Log("ValuableObject component found");
+        SetFieldValue(valuableComponent, "dollarValueOverride", value);
+        SetFieldValue(valuableComponent, "dollarValueOriginal", (float)value);
+        SetFieldValue(valuableComponent, "dollarValueCurrent", (float)value);
+        SetFieldValue(valuableComponent, "dollarValueSet", true);
+
+        var dollarValueRPC = valuableComponent.GetType().GetMethod("DollarValueSetRPC", BindingFlags.Public | BindingFlags.Instance);
+        if (dollarValueRPC != null)
+        {
+            dollarValueRPC.Invoke(valuableComponent, new object[] { (float)value });
+            if (isMultiplayer)
+            {
+                var photonView = valuableComponent.GetComponent<PhotonView>();
+                if (photonView != null)
+                {
+                    photonView.RequestOwnership();
+                    photonView.RPC("DollarValueSetRPC", RpcTarget.Others, (float)value);
+                    photonView.RPC("DiscoverRPC", RpcTarget.All);
+                    photonView.RPC("AddToDollarHaulListRPC", RpcTarget.All);
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("DollarValueSetRPC method not found");
+        }
+    }
+
+    private static void ConfigurePhysicsProperties(GameObject spawnedItem, Vector3 position, bool isMultiplayer)
+    {
+        var physComponent = spawnedItem.GetComponent(Type.GetType("PhysGrabObject, Assembly-CSharp"));
+        if (physComponent == null)
+        {
+            Debug.LogError("PhysGrabObject component not found");
+            return;
+        }
+
+        Debug.Log("PhysGrabObject component found");
+        SetFieldValue(physComponent, "spawnTorque", UnityEngine.Random.insideUnitSphere * 0.05f);
+        if (isMultiplayer)
+        {
+            var photonView = spawnedItem.GetComponent<PhotonView>();
+            photonView?.RPC("SetPositionRPC", RpcTarget.MasterClient, position, Quaternion.identity);
+        }
+    }
+
+    private static void SetFieldValue(object obj, string fieldName, object value)
+    {
+        var field = obj.GetType().GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        if (field != null)
+        {
+            field.SetValue(obj, value);
+        }
+        else
+        {
+            Debug.LogError($"{fieldName} field not found");
+        }
+    }
+
+    private static void EnsureItemVisibility(GameObject item)
+    {
+        item.SetActive(true);
+        foreach (var renderer in item.GetComponentsInChildren<Renderer>(true))
+        {
+            renderer.enabled = true;
+        }
+        item.layer = LayerMask.NameToLayer("Default");
+    }
+}
