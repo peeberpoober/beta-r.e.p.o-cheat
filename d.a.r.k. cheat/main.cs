8f46644704d956fae3bacb10a5d134b0ee74feed
hiccup444 <sunnylilttledragon@gmail.com>
2025-03-19 01:11:10 -0400

    - remove harmony

--- d.a.r.k. cheat/main.cs
+++ d.a.r.k. cheat/main.cs
@@ -1,82 +1,20 @@
-﻿using HarmonyLib;
+using dark_cheat;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace dark_cheat
 {
-
     public class Loader
     {
-        [HarmonyPatch(typeof(SpectateCamera), "PlayerSwitch")]
-        public static class SpectateCamera_PlayerSwitch_Patch
-        {
-            static bool Prefix(bool _next)
-            {
-                if (Hax2.showMenu)
-                {
-                    return false;
-                }
-                return true;
-            }
-        }
-
-        [HarmonyPatch(typeof(Input), "GetMouseButtonUp", new Type[] { typeof(int) })]
-        public class Patch_Input_GetMouseButtonUp
-        {
-            static bool Prefix(int button, ref bool __result)
-            {
-                if (Hax2.showMenu)
-                {
-                    __result = false;
-                    return false;
-                }
-                return true;
-            }
-        }
-        [HarmonyPatch(typeof(Input), "GetMouseButtonDown", new Type[] { typeof(int) })]
-        public class Patch_Input_GetMouseButtonDown
-        {
-            static bool Prefix(int button, ref bool __result)
-            {
-                if (Hax2.showMenu)
-                {
-                    __result = false;
-                    return false;
-                }
-                return true;
-            }
-        }
-
-        [HarmonyPatch(typeof(Input), "GetMouseButton", new Type[] { typeof(int) })]
-        public class Patch_Input_GetMouseButton
-        {
-            static bool Prefix(int button, ref bool __result)
-            {
-                if (Hax2.showMenu)
-                {
-                    __result = false;
-                    return false;
-                }
-                return true;
-            }
-        }
         public static void Init()
         {
             Loader.Load = new GameObject();
             Loader.Load.AddComponent<Hax2>();
             UnityEngine.Object.DontDestroyOnLoad(Loader.Load);
 
-            var Harmony = new Harmony("dark_cheat");
-            try
-            {
-                Harmony.PatchAll();
-                DLog.Log("Harmony patches applied successfully!");
-            }
-            catch (Exception ex)
-            {
-                DLog.Log($"Harmony patches failed to apply: {ex}");
-            }
+            // Previous Harmony patching code removed
+            DLog.Log("Cheat loader initialized successfully!");
         }
 
         private static GameObject Load;
@@ -86,8 +24,7 @@ namespace dark_cheat
             UnityEngine.Object.Destroy(Loader.Load);
             System.GC.Collect();
 
-            var Harmony = new Harmony("dark_cheat");
-            Harmony.UnpatchAll();
+            // Previous Harmony unpatching code removed
         }
     }
 }
